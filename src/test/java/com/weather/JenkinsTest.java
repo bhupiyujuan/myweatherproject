01ed923d34982792feaa9ab34043ceaed3480e40
bhupen <hubbyloveswiffy@gmail.com>
2021-06-12 10:05:17 +0800

    Merge branch 'master' of https://github.com/bhupiyujuan/myweatherproject
    # Conflicts:
    #	README.md

---
+++ src/test/java/com/weather/JenkinsTest.java
@@ -0,0 +1,20 @@
+package com.weather;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+
+public class JenkinsTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @Test
+    public void testHistogramValues() {
+    }
+}
